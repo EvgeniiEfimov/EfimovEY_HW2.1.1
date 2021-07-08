1e783fa5da31d243f58839d2dc82e22ccf68b0b7
1.0.0 <1.0.0>
2021-07-08 12:28:24 +0600

    HW 2.1.1 Finish

--- EfimovEY_HW2.1.1/ViewController.swift
+++ EfimovEY_HW2.1.1/ViewController.swift
@@ -10,18 +10,36 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var redView: UIView!
     @IBOutlet weak var yellowView: UIView!
-    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var greenView: UIView!   
     @IBOutlet weak var startButtenOutlet: UIButton!
-    
-    
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        redView.layer.cornerRadius = 60
+        yellowView.layer.cornerRadius = 60
+        greenView.layer.cornerRadius = 60
         redView.alpha = 0.3
         yellowView.alpha = 0.3
         greenView.alpha = 0.3
     }
 
+    
     @IBAction func startButtenAction() {
+        startButtenOutlet.setTitle("Next", for: .normal)
+        if (redView.alpha == yellowView.alpha && yellowView.alpha == greenView.alpha)
+            || greenView.alpha == CGFloat (1)
+            {
+            redView.alpha = 1
+            greenView.alpha = 0.3
+            yellowView.alpha = 0.3
+        } else if redView.alpha == CGFloat (1) {
+            yellowView.alpha = 1
+            redView.alpha = 0.3
+        } else if yellowView.alpha == CGFloat (1) {
+            yellowView.alpha = 0.3
+            greenView.alpha = 1
+        }
     }
     
 }
